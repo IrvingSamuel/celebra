75ebb29cd2d3c7929599e4d656da4fb6afb7e3ed
Irving Samuel <sync.irvingsamuel@gmail.com>
2026-05-31 23:30:08 -0300

    fix: add VLibras widget to correct layout file (layouts/app instead of components/layouts/app)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -199,5 +199,19 @@
             });
         })();
     </script>
+
+    {{-- VLibras Widget (Acessibilidade - Libras) --}}
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+
+    {{-- VLibras Script --}}
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
 </body>
 </html>
